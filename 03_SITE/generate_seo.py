c6575720866907aec9c6a400be4117d25801ce19
Felipe Ferreira <FelipeFerreira@MacBook-Air-de-Sara.local>
2026-04-07 18:42:56 -0300

    fix(seo): update lastmod date in sitemap and fix generation script to be dynamic

--- 03_SITE/generate_seo.py
+++ 03_SITE/generate_seo.py
@@ -1,7 +1,9 @@
 import os
 import urllib.parse
+from datetime import datetime
 
 base_dir = os.path.dirname(os.path.abspath(__file__))
+current_date = datetime.now().strftime('%Y-%m-%d')
 catA_path = os.path.join(base_dir, 'templates/template_resort.html')
 catB_path = os.path.join(base_dir, 'templates/template_expo.html')
 
@@ -72,13 +74,13 @@ sitemapUrls = """<?xml version="1.0" encoding="UTF-8"?>
 <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
   <url>
     <loc>https://unionmind.solutions/</loc>
-    <lastmod>2026-03-31</lastmod>
+    <lastmod>{current_date}</lastmod>
     <changefreq>weekly</changefreq>
     <priority>1.0</priority>
   </url>
   <url>
     <loc>https://unionmind.solutions/labs.html</loc>
-    <lastmod>2026-03-31</lastmod>
+    <lastmod>{current_date}</lastmod>
     <changefreq>weekly</changefreq>
     <priority>0.8</priority>
   </url>
@@ -88,7 +90,7 @@ def push_sitemap(vid):
     global sitemapUrls
     sitemapUrls += f"""  <url>
     <loc>https://unionmind.solutions/espacos/{vid}.html</loc>
-    <lastmod>2026-03-31</lastmod>
+    <lastmod>{current_date}</lastmod>
     <changefreq>monthly</changefreq>
     <priority>0.7</priority>
   </url>\n"""
